b809b25ec5a53bd9061dc732326f73a5d686c017
FranklinCODEX <lawsonlatevi005@gmail.com>
2023-12-08 17:47:57 +0100

    Probleme avec un package

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,17 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *     title="Application de don en ligne",
- *     version="1.0.0",
- *     description="API du projet de don en ligne ",
- *     @OA\Contact(
- *         email="zinmonsesylvie@gmail.com",
- *         name="ZINMONSE Sylvie"
- *     ),
- * )
- */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
